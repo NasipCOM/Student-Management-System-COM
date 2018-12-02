449a5c953ce943ace70616f13056fbe87734348f
NasipCOM <38931214+NasipCOM@users.noreply.github.com>
2018-12-02 21:36:13 +0600

    #

--- src/main/DatabaseHandler.java
+++ src/main/DatabaseHandler.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseHandler exdends Configs{
+public class DatabaseHandler extends Configs{
     private Connection dbConnection;
 
     public Connection getDbConnectino()

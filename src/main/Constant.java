1538f3c8e5165d95394a340e147ac9e068ff4c7e
NasipCOM <38931214+NasipCOM@users.noreply.github.com>
2018-12-09 12:11:09 +0600

    Db request

--- src/main/Constant.java
+++ src/main/Constant.java
@@ -2,7 +2,7 @@ package main;
 
 public class Constant {
         public static final String USER_TABLE = "users";
-        public static final String USER_ID ="iduser";
+        public static final String USER_ID ="userid";
         public static final String USER_FIRSTNAME = "username";
         public static final String USER_LASTNAME = "usersurname";
         public static final String USER_PASSWORD = "userpassword";
--- src/main/DatabaseHandler.java
+++ src/main/DatabaseHandler.java
@@ -2,12 +2,13 @@ package main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseHandler extends Configs{
     private Connection dbConnection;
 
-    public Connection getDbConnectino()
+    public Connection getDbConnection()
         throws ClassNotFoundException, SQLException{
         String connectionString = "jdbc:mysql//" + dbHost + ":"
                 + dbPort + "/" + dbName;
@@ -18,4 +19,23 @@ public class DatabaseHandler extends Configs{
                 dbUser, dbPass);
         return dbConnection;
     }
+
+public void signUpUser(String firstName, String lastName, String password, int userId) {
+    String insert = "INSERT INTO" + Constant.USER_TABLE + "(" +
+            Constant.USER_ID + "," + Constant.USER_FIRSTNAME + " , " +
+            Constant.USER_LASTNAME + " ," + Constant.USER_PASSWORD + ")" +
+            "VALUES(?,?,?,?)";
+    PreparedStatement prSt = getDbConnection().prepareStatement(insert);
+    prSt.setString(1, firstName);
+    prSt.setInt(2, userId);
+    prSt.setString(3,lastName);
+    prSt.setString(4, password);
+
+    try {
+        prSt.executeUpdate();
+    }catch (SQLException e){
+        e.printStackTrace();
+    }
+
+}
 }
